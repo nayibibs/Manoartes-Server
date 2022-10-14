5731b8533dc5921b2e610e8373bedce36143176e
nayibibs <nayibibs@gmail.com>
2022-10-14 09:59:58 -0400

    :construction:

--- models/User.model.js
+++ models/User.model.js
@@ -22,7 +22,7 @@ const userSchema = new Schema(
   },
  
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
+    // this  second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
